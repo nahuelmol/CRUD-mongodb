2ff16844ebd208632f89642c36936accff90e6c4
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-18 16:03:43 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
+const indexRoutes = require('./routes/index');
+const apiRoutes   = require('./routes/api_routes');
+
 const app = express();
 
 const hostname = '192.168.0.101';
@@ -16,7 +19,6 @@ mongoose.connect('mongodb://localhost/crud-mongo',options)
 	.then(db => console.log(`db conectada -> ${db}`))
 	.catch(err => console.log(`se presento el siguiente error -> ${err}`));
 
-const indexRoutes = require('./routes/index');
 
 app.use('/static', express.static(path.join(__dirname,'public')));
 app.use(morgan('dev'));
@@ -26,7 +28,8 @@ app.set('port',process.env.PORT || 3000);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-app.use('/',indexRoutes);
+app.use('/',     indexRoutes);
+app.use('/api/', apiRoutes);
 
 
 module.exports = app;
