86a4aa4c1aa31fb79fa023a7522196353c3f8248
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-18 16:17:10 -0300

    Update api_routes.js

--- src/routes/api_routes.js
+++ src/routes/api_routes.js
@@ -1,1 +1,15 @@
+const { Author, Book } = require('../models/index');
 
+app.get('author/:authorId', (req,res) => {
+        Author.findById(req.params.Id).then((result) => {
+            console.log(result);
+            res.status(200).send(result);
+        })
+});
+
+app.get('book/:bookId', (req,res) => {
+        Book.findById(req.params.Id).then((result) => {
+            console.log(result);
+            res.status(200).send(result);
+        })
+});
